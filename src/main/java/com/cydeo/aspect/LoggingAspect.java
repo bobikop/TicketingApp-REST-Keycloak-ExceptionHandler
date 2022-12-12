1501974b155724ee56dc516494bcea5b55846efd
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-12-12 18:38:25 -0500

    TicketingApp Rest

---
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,52 @@
+package com.cydeo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+//    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    private String getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SimpleKeycloakAccount userDetails = (SimpleKeycloakAccount) authentication.getDetails();
+        return userDetails.getKeycloakSecurityContext().getToken().getPreferredUsername();
+    }
+
+    @Pointcut("execution(* com.cydeo.controller.ProjectController.*(..)) || execution(* com.cydeo.controller.TaskController.*(..))")
+    public void anyProjectAndTaskControllerPC() {}
+
+    @Before("anyProjectAndTaskControllerPC()")
+    public void beforeAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint) {
+        log.info("Before -> Method: {}, User: {}"
+        , joinPoint.getSignature().toShortString()
+        , getUsername());
+    }
+
+    @AfterReturning(pointcut = "anyProjectAndTaskControllerPC()", returning = "results")
+    public void afterReturningAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint, Object results) {
+        log.info("After Returning -> Method: {}, User: {}, Results: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername()
+                , results.toString());
+    }
+
+    @AfterThrowing(pointcut = "anyProjectAndTaskControllerPC()", throwing = "exception")
+    public void afterReturningAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint, Exception exception) {
+        log.info("After Returning -> Method: {}, User: {}, Results: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername()
+                , exception.getMessage());
+    }
+
+}
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.annotation.ExecutionTime;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.exception.TicketingProjectException;
@@ -24,6 +25,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ExecutionTime
     @GetMapping
     @RolesAllowed({"Manager","Admin"})
     @Operation(summary = "Get users")
@@ -32,6 +34,7 @@ public class UserController {
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieves",userDTOList, HttpStatus.OK));
     }
 
+    @ExecutionTime
     @GetMapping("/{username}")
     @RolesAllowed("Admin")
     @Operation(summary = "Get user by username")
