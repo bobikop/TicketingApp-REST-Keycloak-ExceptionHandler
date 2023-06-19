db78c5db7f74727a26fc33a6fac43d1681fca105
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-18 23:12:04 -0400

    Exception

--- src/main/java/com/cydeo/annotation/DefaultExceptionMessage.java
+++ src/main/java/com/cydeo/annotation/DefaultExceptionMessage.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
+
+// this is how we can create our custom annotation in Java
+// first we add @Target second @Retention
+@Target(ElementType.METHOD) // we will put annotation above method
+@Retention(RetentionPolicy.RUNTIME) // once the exception happen in the method our custom exception will be executed
 public @interface DefaultExceptionMessage {
 
     String defaultMessage() default "";
